fcd67a9030deb49640f040d5c6eb6a71fa241f87
DAguileraBusselle <78724490+DAguileraBusselle@users.noreply.github.com>
2021-11-09 12:34:23 +0100

    calc primer commit

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnMas = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnDec = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblResultado.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.White;
+            this.lblResultado.Location = new System.Drawing.Point(13, 19);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(358, 79);
+            this.lblResultado.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(20, 114);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(85, 60);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn2.Location = new System.Drawing.Point(115, 114);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(85, 60);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn3.Location = new System.Drawing.Point(210, 114);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(85, 60);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn4.Location = new System.Drawing.Point(20, 184);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(85, 60);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn5.Location = new System.Drawing.Point(115, 184);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(85, 60);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn6.Location = new System.Drawing.Point(210, 184);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(85, 60);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn7.Location = new System.Drawing.Point(20, 254);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(85, 60);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn8.Location = new System.Drawing.Point(115, 254);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(85, 60);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn9.Location = new System.Drawing.Point(210, 254);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(85, 60);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btn10.Location = new System.Drawing.Point(115, 324);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(85, 60);
+            this.btn10.TabIndex = 10;
+            this.btn10.Text = "0";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btnEnter.Location = new System.Drawing.Point(210, 324);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(85, 60);
+            this.btnEnter.TabIndex = 11;
+            this.btnEnter.Text = "=";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Font = new System.Drawing.Font("Trebuchet MS", 30F, System.Drawing.FontStyle.Bold);
+            this.btnMenos.Location = new System.Drawing.Point(301, 114);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(66, 60);
+            this.btnMenos.TabIndex = 12;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click);
+            // 
+            // btnMas
+            // 
+            this.btnMas.Font = new System.Drawing.Font("Trebuchet MS", 30F, System.Drawing.FontStyle.Bold);
+            this.btnMas.Location = new System.Drawing.Point(301, 184);
+            this.btnMas.Name = "btnMas";
+            this.btnMas.Size = new System.Drawing.Size(65, 60);
+            this.btnMas.TabIndex = 13;
+            this.btnMas.Text = "+";
+            this.btnMas.UseVisualStyleBackColor = true;
+            this.btnMas.Click += new System.EventHandler(this.btnMas_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btnMult.Location = new System.Drawing.Point(301, 254);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(65, 60);
+            this.btnMult.TabIndex = 14;
+            this.btnMult.Text = "x";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btnDiv.Location = new System.Drawing.Point(301, 324);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(65, 60);
+            this.btnDiv.TabIndex = 15;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnDec
+            // 
+            this.btnDec.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold);
+            this.btnDec.Location = new System.Drawing.Point(20, 324);
+            this.btnDec.Name = "btnDec";
+            this.btnDec.Size = new System.Drawing.Size(85, 60);
+            this.btnDec.TabIndex = 16;
+            this.btnDec.Text = ",";
+            this.btnDec.UseVisualStyleBackColor = true;
+            this.btnDec.Click += new System.EventHandler(this.btnRest_Click);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(35, 398);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 26);
+            this.label1.TabIndex = 17;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(384, 439);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDec);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnMas);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblResultado);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "**CALCULADORA**";
+            this.Text = "**CALCULADORA**";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnMas;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnDec;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private static int contOperadores = 0;
+        private static String textPantalla = "";
+        private static int contMuerte = 0;
+        private static int centinelaRes = 0;
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "1";
+            lblResultado.Text = textPantalla;
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "2";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "3";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "4";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "5";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "6";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "7";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "8";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "9";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += "0";
+            lblResultado.Text = textPantalla;
+
+
+        }
+
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            if (contOperadores < 1)
+            {
+                textPantalla += " - ";
+                lblResultado.Text = textPantalla;
+                contOperadores++;
+            } else
+            {
+                label1.Text = "solo puede realizar una operacion";
+            }
+            
+
+
+        }
+
+        private void btnMas_Click(object sender, EventArgs e)
+        {
+            if (contOperadores < 1)
+            {
+                textPantalla += " + ";
+                lblResultado.Text = textPantalla;
+                contOperadores++;
+            } else
+            {
+                label1.Text = "solo puede realizar una operacion";
+            }
+
+
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            if (contOperadores < 1)
+            {
+                textPantalla += " x ";
+                lblResultado.Text = textPantalla;
+                contOperadores++;
+            } else
+            {
+                label1.Text = "solo puede realizar una operacion";
+            }
+
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            if (contOperadores < 1)
+            {
+                textPantalla += " / ";
+                lblResultado.Text = textPantalla;
+                contOperadores++;
+            } else
+            {
+                label1.Text = "solo puede realizar una operacion";
+            }
+
+
+        }
+
+        private void btnRest_Click(object sender, EventArgs e)
+        {
+            lblResultado.ForeColor = Color.White;
+
+            textPantalla += ",";
+            lblResultado.Text = textPantalla;
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+
+            string[] operacion = textPantalla.Split(' ');
+            int n = 0;
+            Double num1 = 0;
+            Double num2 = 0;
+            string operador = "";
+
+            double resultado = 0;
+
+
+            foreach (string word in operacion)
+            {
+                if (n==0)
+                {
+                    Console.WriteLine(word);
+
+                    num1 = Double.Parse(word);
+                    Console.WriteLine(num1);
+                    n++;
+                    
+                    
+                } else if (n==1)
+                {
+                    Console.WriteLine(word);
+
+                    operador = word;
+                    n++;
+                } else if (n==2)
+                {
+                    Console.WriteLine(word);
+                    
+                    num2 = Double.Parse(word);
+                    
+                    Console.WriteLine(num2);
+                    n++;
+                    
+                    
+                }
+            }
+
+            switch (operador)
+            {
+                case  "-":
+                    resultado = num1 - num2;
+                    Console.WriteLine(resultado);
+
+                    break;
+
+                case "+":
+                    resultado = num1 + num2;
+                    Console.WriteLine(resultado);
+
+                    break;
+
+                case "x":
+                    resultado = num1 * num2;
+                    Console.WriteLine(resultado);
+
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    Console.WriteLine(resultado);
+
+                    break;
+
+            }
+
+            if (contMuerte == 5)
+            {
+                lblResultado.ForeColor = Color.Red;
+                
+                lblResultado.Text = "No";
+            } else
+            {
+                lblResultado.ForeColor = Color.White;
+                lblResultado.Text = resultado.ToString();
+            }
+
+            
+            textPantalla = "";
+            contOperadores = 0;
+            label1.Text = "";
+            contMuerte++;
+
+        }
+
+        
+       
+    }
+}
